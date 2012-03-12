42a6035330c03cad7fb2bd699d7a4ed9213631c5
Andrew Bredow <bredow@gmail.com>
2012-03-11 22:15:14 -0500

    Added a simple build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+include=(
+    background_scripts 
+    content_scripts
+    icons
+    library
+    manifest.json
+    popups
+)
+
+rm build/library_detector.zip
+mkdir build/library_detector
+
+for path in ${include[@]}
+do
+    cp -R $path build/library_detector
+done
+
+cd build
+zip -r library_detector.zip library_detector
+rm -R library_detector

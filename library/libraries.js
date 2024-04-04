9b3f7f31fafb589f767182da709825b75f465dde
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-04-04 12:48:51 -0700

    Add Svelte and SvelteKit

--- library/libraries.js
+++ library/libraries.js
@@ -1971,5 +1971,39 @@ var d41d8cd98f00b204e9800998ecf8427e_LibraryDetectorTests = {
         }
         return false;
       }
+    },
+    'Svelte': {
+      id: 'svelte',
+      icon: 'svelte',
+      url: 'https://svelte.dev/',
+      npm: 'svelte',
+      test: function (win) {
+        // can be toggled with discloseVersion option
+        // see https://svelte.dev/docs/svelte-compiler#types-compileoptions
+        const versions = win.__svelte.v;
+        if (versions) {
+          if (versions.size === 1) {
+            return { version: versions.values().next().value };
+          } else {
+            return { version: UNKNOWN_VERSION };
+          }
+        }
+        if (document.querySelector('[class*="svelte-"]')) {
+          return { version: UNKNOWN_VERSION };
+        }
+        return false;
+      }
+    },
+    'SvelteKit': {
+      id: 'sveltekit',
+      icon: 'svelte',
+      url: 'https://kit.svelte.dev/',
+      npm: '@sveltejs/kit',
+      test: function (win) {
+        if (document.body.innerHTML.includes('__sveltekit_')) {
+          return { version: UNKNOWN_VERSION };
+        }
+        return false;
+      }
     }
 };
